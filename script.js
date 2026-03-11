8e161d91b873edb74c618004359ee9482c023699
Harsh Arya <harsharya8946@gmail.com>
2026-03-11 23:04:00 +0530

    making snake

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
-let board = document.querySelector('.board');
-let blockheight = 30;
-let blockwidth = 30;
-let cols = Math.floor(board.clientHeight / blockheight);
-let rows = Math.floor(board.clientWidth / blockwidth);
+const board = document.querySelector('.board');
+
+const blockheight = 30;
+const blockwidth = 30;
+
+const rows = Math.floor(board.clientHeight / blockheight);
+const cols = Math.floor(board.clientWidth / blockwidth);
+
+let blocks = [];
+
+let snake = [
+    {x:5,y:5},
+    {x:5,y:6},
+    {x:5,y:7}
+];
 
 for(let row = 0; row < rows; row++){
-    for(let col =0; col < cols; col++){
-        let block = document.createElement('div');
+    for(let col = 0; col < cols; col++){
+        const block = document.createElement('div');
         block.classList.add('block');
         board.appendChild(block);
+
+        blocks.push(block);
     }
 }
+
+function render(){
+    snake.forEach(segment =>{
+        const index = segment.x * cols + segment.y;
+        blocks[index].classList.add('fill');
+    })
+}
+render();
